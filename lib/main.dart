d3b54c4dd7de81050b688c747ba634fc55d40d4f
dani <danielabera285@gmail.com>
2025-02-03 17:10:26 +0300

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
+import 'package:azure_quiz_study_app/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // Ensure Flutter bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Run the app
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AzureIQ',
-      theme: ThemeData(),
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
